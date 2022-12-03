7c5f5e3c2fa0d539f38495ce1b9f554d98c2b54c
ryan weiss <ryanweiss02@gmail.com>
2022-12-02 21:59:24 -0500

    database export

--- database/netconnect.sql
+++ database/netconnect.sql
@@ -1,130 +1,184 @@
--- DROP SCHEMA public;
+--
+-- PostgreSQL database dump
+--
 
-CREATE SCHEMA public AUTHORIZATION postgres;
+-- Dumped from database version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.5 (Ubuntu 14.5-0ubuntu0.22.04.1)
 
--- DROP SEQUENCE public.accounts_id_seq;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
-CREATE SEQUENCE public.accounts_id_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
+--
+-- Name: date_updated(); Type: FUNCTION; Schema: public; Owner: postgres
+--
 
--- Permissions
-
-ALTER SEQUENCE public.accounts_id_seq OWNER TO postgres;
-GRANT ALL ON SEQUENCE public.accounts_id_seq TO postgres;
-
--- DROP SEQUENCE public.connections_id_seq;
-
-CREATE SEQUENCE public.connections_id_seq
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.connections_id_seq OWNER TO postgres;
-GRANT ALL ON SEQUENCE public.connections_id_seq TO postgres;
-
--- DROP SEQUENCE public.connections_id_seq1;
-
-CREATE SEQUENCE public.connections_id_seq1
-	INCREMENT BY 1
-	MINVALUE 1
-	MAXVALUE 2147483647
-	START 1
-	CACHE 1
-	NO CYCLE;
-
--- Permissions
-
-ALTER SEQUENCE public.connections_id_seq1 OWNER TO postgres;
-GRANT ALL ON SEQUENCE public.connections_id_seq1 TO postgres;
--- public.accounts definition
-
--- Drop table
-
--- DROP TABLE accounts;
-
-CREATE TABLE accounts (
-	id serial4 NOT NULL,
-	username varchar NULL,
-	"password" varchar NULL,
-	"name" varchar NULL,
-	email varchar NULL,
-	phone_number varchar NULL,
-	birthdate date NULL,
-	gender varchar NULL,
-	status varchar NULL,
-	school varchar NULL,
-	concentration varchar NULL,
-	bio varchar NULL,
-	date_created timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-	date_updated timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-	enabled bool NOT NULL DEFAULT true,
-	CONSTRAINT id UNIQUE (id),
-	CONSTRAINT username UNIQUE (username)
-);
-
--- Table Triggers
-
-create trigger date_updated before
-update
-    on
-    public.accounts for each row execute function date_updated();
-
--- Permissions
-
-ALTER TABLE public.accounts OWNER TO postgres;
-GRANT ALL ON TABLE public.accounts TO postgres;
-
-
--- public.connections definition
-
--- Drop table
-
--- DROP TABLE connections;
-
-CREATE TABLE connections (
-	id serial4 NOT NULL,
-	"user" int8 NOT NULL,
-	follower int8 NOT NULL,
-	CONSTRAINT follower_fk FOREIGN KEY (follower) REFERENCES accounts(id),
-	CONSTRAINT user_fk FOREIGN KEY ("user") REFERENCES accounts(id)
-);
-
--- Permissions
-
-ALTER TABLE public.connections OWNER TO postgres;
-GRANT ALL ON TABLE public.connections TO postgres;
-
-
-
-CREATE OR REPLACE FUNCTION public.date_updated()
- RETURNS trigger
- LANGUAGE plpgsql
-AS $function$
+CREATE FUNCTION public.date_updated() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
 	begin
 		NEW.date_updated = CURRENT_TIMESTAMP;
 	    RETURN NEW;
 	END;
-$function$
-;
+$$;
 
--- Permissions
 
 ALTER FUNCTION public.date_updated() OWNER TO postgres;
-GRANT ALL ON FUNCTION public.date_updated() TO public;
-GRANT ALL ON FUNCTION public.date_updated() TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.accounts (
+    id integer NOT NULL,
+    username character varying,
+    password character varying,
+    name character varying,
+    email character varying,
+    phone_number character varying,
+    birthdate date,
+    gender character varying,
+    status character varying,
+    school character varying,
+    concentration character varying,
+    bio character varying,
+    date_created timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    date_updated timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    enabled boolean DEFAULT true NOT NULL
+);
 
 
--- Permissions
+ALTER TABLE public.accounts OWNER TO postgres;
 
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
+--
+-- Name: accounts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.accounts_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.accounts_id_seq OWNER TO postgres;
+
+--
+-- Name: accounts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.accounts_id_seq OWNED BY public.accounts.id;
+
+
+--
+-- Name: connections; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.connections (
+    id integer NOT NULL,
+    "user" bigint NOT NULL,
+    follower bigint NOT NULL
+);
+
+
+ALTER TABLE public.connections OWNER TO postgres;
+
+--
+-- Name: connections_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.connections_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.connections_id_seq OWNER TO postgres;
+
+--
+-- Name: connections_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.connections_id_seq OWNED BY public.connections.id;
+
+
+--
+-- Name: accounts id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts ALTER COLUMN id SET DEFAULT nextval('public.accounts_id_seq'::regclass);
+
+
+--
+-- Name: connections id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.connections ALTER COLUMN id SET DEFAULT nextval('public.connections_id_seq'::regclass);
+
+
+--
+-- Name: accounts accounts_unique_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_unique_id UNIQUE (id);
+
+
+--
+-- Name: accounts accounts_unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_unique_username UNIQUE (username);
+
+
+--
+-- Name: connections connections_unique_id; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.connections
+    ADD CONSTRAINT connections_unique_id UNIQUE (id);
+
+
+--
+-- Name: accounts date_updated; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER date_updated BEFORE UPDATE ON public.accounts FOR EACH ROW EXECUTE FUNCTION public.date_updated();
+
+
+--
+-- Name: connections follower_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.connections
+    ADD CONSTRAINT follower_fk FOREIGN KEY (follower) REFERENCES public.accounts(id);
+
+
+--
+-- Name: connections user_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.connections
+    ADD CONSTRAINT user_fk FOREIGN KEY ("user") REFERENCES public.accounts(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
